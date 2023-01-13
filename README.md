07e8bb78c9cfb570bbef6404b768968c45dccb90
choewy <choewy32@gmail.com>
2023-01-14 04:44:37 +0900

    install react-navigation

--- README.md
+++ README.md
@@ -14,3 +14,26 @@ $ npx react-native init Delivery --template react-native-template-typescript
 $ cd Delivery/ios
 $ pod install
 ```
+
+## Navigation
+
+```
+$ npm i @react-navigation/native @react-navigation/native-stack react-native-screens react-native-safe-area-context
+```
+
+- additional(Mac)
+
+```
+$ npx pod-install
+```
+
+./android/app/src/main/java/Delivery/MainActivity.java
+
+```java
+import android.os.Bundle;
+
+@Override
+protected void onCreate(Bundle saveInstanceState) {
+  super.onCreate(null);
+}
+```
--- android/app/src/main/java/com/delivery/MainActivity.java
+++ android/app/src/main/java/com/delivery/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +15,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Delivery";
+  }
+
+  /**
+   * React Native Navigation
+   * rendering of the component.
+   */
+  @Override
+  protected void onCreate(Bundle saveInstanceState) {
+    super.onCreate(null);
   }
 
   /**
