b6af57e92b1bf7e4573a46b41120bfd541e4c677
choewy <choewy32@gmail.com>
2023-01-15 18:21:22 +0900

    use public metadata

--- delivery-app-server/src/app/app.controller.ts
+++ delivery-app-server/src/app/app.controller.ts
@@ -1,7 +1,9 @@
+import { Public } from '@/core';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Public()
   @Get('health')
   async healthCheck(): Promise<string> {
     return 'complete';
