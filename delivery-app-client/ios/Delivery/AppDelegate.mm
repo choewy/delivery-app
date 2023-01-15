7d40ce934ad6a1db5fcde3c101a75dcfa61c9811
choewy <choewy32@gmail.com>
2023-01-15 17:22:30 +0900

    clear key chain in IOS - encrypted-storage

--- delivery-app-client/ios/Delivery/AppDelegate.mm
+++ delivery-app-client/ios/Delivery/AppDelegate.mm
@@ -2,10 +2,38 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+/**
+ Deletes all Keychain items accessible by this app if this is the first time the user launches the app
+ */
+static void ClearKeychainIfNecessary() {
+    // Checks wether or not this is the first time the app is run
+    if ([[NSUserDefaults standardUserDefaults] boolForKey:@"HAS_RUN_BEFORE"] == NO) {
+        // Set the appropriate value so we don't clear next time the app is launched
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"HAS_RUN_BEFORE"];
+
+        NSArray *secItemClasses = @[
+            (__bridge id)kSecClassGenericPassword,
+            (__bridge id)kSecClassInternetPassword,
+            (__bridge id)kSecClassCertificate,
+            (__bridge id)kSecClassKey,
+            (__bridge id)kSecClassIdentity
+        ];
+
+        // Maps through all Keychain classes and deletes all items that match
+        for (id secItemClass in secItemClasses) {
+            NSDictionary *spec = @{(__bridge id)kSecClass: secItemClass};
+            SecItemDelete((__bridge CFDictionaryRef)spec);
+        }
+    }
+}
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // Add this line to call the above function
+  ClearKeychainIfNecessary();
+
   self.moduleName = @"Delivery";
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
